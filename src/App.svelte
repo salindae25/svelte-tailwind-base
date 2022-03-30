4e1cec881d342e260ef2fb5d34251ac07c031a72
salinda c ekanayake <sal234@salindas-MacBook-Air.local>
2022-03-30 20:28:26 +0530

    added all files with routes

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+   import { Router, Link, Route } from "svelte-routing";
+   import Home from "./routes/Home.svelte";
+  import Blog from "./routes/Blog.svelte";
+  export let url = "";
+</script>
+
+<Router url="{url}">
+  <div class="flex flex-col">
+  <nav class="bg-gray-200 px-10 py-5 flex gap-10">
+    <Link to="/">Home</Link>
+    <Link to="blog">Blog</Link>
+  </nav>
+  <div class="px-24">
+    <Route path="blog" component="{Blog}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</div>
+</Router>
---
+++ src/routes/Blog.svelte
@@ -0,0 +1,6 @@
+<main class="flex flex-col">
+<h2 class="text-xl font-medium mx-auto mt-24">Blog</h2>
+
+<p class="mt-10">Welcome to Blog page</p>
+
+</main>
